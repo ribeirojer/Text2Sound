a3995a0be33191687ec49fd6512b01ad23aa8d18
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-08-09 11:32:52 -0300

    feat: edit convert audio, init upload to database

--- src/pages/api/convert.ts
+++ src/pages/api/convert.ts
@@ -1,8 +1,8 @@
-// pages/api/convert.js
 import { NextApiRequest, NextApiResponse } from 'next';
 import { OpenAI } from 'openai';
 import fs from 'fs';
 import path from 'path';
+import { uploadAudioFile } from '@/utils/supabase';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,7 +10,7 @@ export default async function handler(
   ) {  if (req.method === 'POST') {
     const { text } = req.body;
     const now = new Date();
-    const speechFile = path.resolve(__dirname, './speech' + now.getTime() + '.mp3');
+    const speechFile = path.resolve('./speech' + now.getTime() + '.mp3');
 
     if (!text) {
       return res.status(400).json({ error: 'Text is required' });
@@ -22,19 +22,20 @@ export default async function handler(
       const response = await openai.audio.speech.create({
         input: text,
         model: "tts-1",
-        voice: 'alloy', // Ajuste conforme necessário
+        voice: 'alloy',
       });
-
-      const audioUrl = response.body;
-
+      
+      const buffer = Buffer.from(await response.arrayBuffer());
+      
+      console.log(`Streaming response to ${speechFile}`);
+      await fs.promises.writeFile(speechFile, buffer);
+      console.log('Finished streaming');
+      
+      const audioUrl = await uploadAudioFile(speechFile)
+           
       if(!audioUrl){
         return
       }
-
-      console.log(`Streaming response to ${speechFile}`);
-      await streamToFile(audioUrl, speechFile);
-      console.log('Finished streaming');
-    
       res.status(200).json({ audioUrl });
     } catch (error) {
       console.error('Error processing text:', error);
@@ -44,15 +45,3 @@ export default async function handler(
     res.status(405).json({ message: 'Method not allowed' });
   }
 };
-
-async function streamToFile(stream: any, path: fs.PathLike) {
-    return new Promise((resolve, reject) => {
-      const writeStream = fs.createWriteStream(path).on('error', reject).on('finish', resolve);
-  
-      // If you don't see a `stream.pipe` method and you're using Node you might need to add `import 'openai/shims/node'` at the top of your entrypoint file.
-      stream.pipe(writeStream).on('error', (error: any) => {
-        writeStream.close();
-        reject(error);
-      });
-    });
-  }
