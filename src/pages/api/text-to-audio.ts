663172950bb96d10a684af25c2f089392e4abb99
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2024-08-25 18:06:53 -0300

    feat: edit textParts of text conversion

--- src/pages/api/text-to-audio.ts
+++ src/pages/api/text-to-audio.ts
@@ -11,6 +11,8 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const { text, id } = req.body;
+    console.log(text);
+    console.log(id);
     const now = new Date();
 
     if (!text) {
@@ -20,38 +22,57 @@ export default async function handler(
     try {
       let audioUrls: string[] = [];
       let wordTimings: any[] = [];
+
+      // Dividir o texto em parágrafos
+      const paragraphs = text.split(/\n+/);
+
+      let textParts: string[] = [];
+      let currentPart = "";
+
+      for (const paragraph of paragraphs) {
+        if ((currentPart + paragraph).length <= MAX_TEXT_LENGTH) {
+          // Adiciona o parágrafo atual à parte atual
+          currentPart += (currentPart ? "\n" : "") + paragraph;
+        } else {
+          // Adiciona a parte atual à lista de partes e inicia uma nova parte com o parágrafo atual
+          if (currentPart) textParts.push(currentPart);
+          currentPart = paragraph;
+        }
+      }
+
+      // Adiciona a última parte, se houver
+      if (currentPart) textParts.push(currentPart);
       
-      const textParts = text.match(new RegExp(`.{1,${MAX_TEXT_LENGTH}}`, 'g')) || [];
-
-      for (const [index, part] of textParts.entries()) {
+      textParts.forEach(async (part, index) => {
         const speechFile = `./audios/speech${now.getTime()}_${index}.mp3`;
-
+      
         const response = await convertTextToSpeech(part);
-
+      
         if (!response.ok) {
           res.status(500).json({ error: "Error converting text to speech" });
           return;
         }
-
+      
         const buffer = Buffer.from(await response.arrayBuffer());
         const audioUrl = await uploadAudioFile(speechFile, buffer);
-
+      
         if (!audioUrl) {
           res.status(500).json({ error: "Error uploading audio file" });
           return;
         }
-
+      
         audioUrls.push(audioUrl);
-
-/**
+      
+        /**
         const timings = await createWordTimings(audioUrl);
         if (timings) {
           wordTimings.push(...timings);
         } */
+      });
+      
+      if (id) {
+        await saveAudioUrl(id, audioUrls);
       }
-        if (id) {
-          await saveAudioUrl(id, audioUrls);
-        }
 
       res.status(200).json({ audioUrls, wordTimings });
     } catch (error) {
